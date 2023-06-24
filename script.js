1b668f6ccfadbf6e9d505439416e0516a2b4ea84
Jay Termulo <jetermulo@gmail.com>
2023-06-25 02:57:13 +0800

    modify JS

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
-// VARIABLE
-const name = document.querySelector(`CSS-Selector`);
-// EVENT LISTENER
-Node.addEventListener('event-name', callback-func);
-// FUNCTION
-function myFunction(a, b) {
-    return a + b ;
+const form = document.getElementById("form"),
+    username = document.getElementById("username"),
+    email = document.getElementById("email"),
+    password = document.getElementById("password"),
+    password2 = document.getElementById("password2");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    checkRequired([username, email, password, password2]);
+});
+
+// Check Required Fields
+function checkRequired(inputAll) {
+    inputAll.forEach( (input) =>{
+        if(input.value.trim() === ""){
+            showError(input, `${getFieldName()} is required`);
+        } else {
+            showSuccess(input);
+        }
+    })
 }
-myFunction(1, 2); /* answer: 3*/
+
+// Show Error Message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control error";
+    const small = formControl.querySelector("small");
+    small.innerText = message;
+}
+
+// Show Success Message 
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
+
+// Get Field Name
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
 
